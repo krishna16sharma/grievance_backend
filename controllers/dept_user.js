935af590a891a2eca958632a5c2b652c171169b4
Krishna Sharma <krishna16sharma.s@gmail.com>
2020-11-19 20:00:43 +0530

    Update dept_user.js

--- controllers/dept_user.js
+++ controllers/dept_user.js
@@ -21,6 +21,50 @@ const handleDept = (req,res,db) =>{
             )
         }
     )
+    .catch(state=>{
+        db.select('department_user_id','department_id').from('department_user')
+        .where('person_id','=',user.person_id)
+        .then(
+            d_id =>{
+                console.log("D_User",d_id[0]);
+                db.select('grievance_id').from('dept_handles')
+                .where('department_id','=',d_id[0].department_id)
+                .then(
+                    grievances =>{
+                        console.log('Grievances', grievances, grievances[0])
+                        db.select('*').from('grievance')
+                        .where('grievance_id','ilike','%'+d_id[0].department_id+'%')
+                        .then(details =>{
+                            console.log('Retrieved!', details, details[0])
+                            res.json(details)
+                        })
+                    }
+                )
+            }
+        )
+        .catch(ut=>{
+                db.select('ut_user_id','ut_abbreviation').from('union_territory_user')
+                .where('person_id','=',user.person_id)
+                .then(
+                    d_id =>{
+                        console.log("D_User",d_id[0]);
+                        db.select('grievance_id').from('ut_handles')
+                        .where('ut_abbreviation','=',d_id[0].ut_abbreviation)
+                        .then(
+                            grievances =>{
+                                console.log('Grievances', grievances, grievances[0])
+                                db.select('*').from('grievance')
+                                .where('grievance_id','ilike','%'+d_id[0].ut_abbreviation+'%')
+                                .then(details =>{
+                                    console.log('Retrieved!', details, details[0])
+                                    res.json(details)
+                                })
+                            }
+                        )
+                    }
+                ).catch(err => res.status(400).json('Unable to retrieve data!'))
+        })
+    })
     .catch(err => res.status(400).json('Unable to retrieve data!'))
 }
 
