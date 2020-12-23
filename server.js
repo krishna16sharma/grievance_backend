67a48d913930acf907d323251a9f310fa3893c8a
Krishna Sharma <krishna16sharma.s@gmail.com>
2020-12-23 08:19:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const register = require('./controllers/register');
 const aadhaar = require('./controllers/aadhaar');
 const g_form = require('./controllers/grievance_form');
 const dashboard = require('./controllers/dashboard');
+const adm_dashboard = require('./controllers/adm_dashboard');
+const dept_user = require('./controllers/dept_user');
 const response = require('./controllers/response');
+const webtraffic = require('./controllers/webtraffic');
+const newresponse = require('./controllers/new_response');
 
 const db = knex({
   client: 'pg',
@@ -36,6 +40,11 @@ app.post('/aadhaar', (req,res) => { aadhaar.handleAadhaar(req,res,db)} )
 app.post('/grievance_submit', (req,res) => { g_form.handleGrievance(req,res,db)})
 app.post('/dashboard', (req,res) => { dashboard.handleDashboard(req,res,db)})
 app.post('/response', (req,res) => { response.handleResponse(req,res,db)} )
+app.post('/adm_dashboard', (req,res) => { adm_dashboard.handleAdmin(req,res,db)} )
+app.post('/dept_user', (req,res) => { dept_user.handleDept(req,res,db)} )
+app.post('/webtraffic', (req,res) => { webtraffic.handleWebTraffic(req,res,db)} )
+app.post('/new_response', (req,res) => { newresponse.handleNewResponse(req,res,db)} )
+
 
 app.listen( 3000 , ()=>{
     console.log('Server running on port 3000');
